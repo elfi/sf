89c87f427dcc4b163db644c30a15d2dba0f695b2
Miroslav Dobsicek <m.dobsicek@gmail.com>
2013-04-07 10:41:11 +0200

    nanb and andb3

--- Basics.v
+++ Basics.v
@@ -41,7 +41,6 @@ Definition andb (b1:bool) (b2:bool) : bool :=
     | false => false
     end.
 
-
 Definition orb (b1:bool) (b2:bool) : bool :=
     match b1 with
     | true => true
@@ -60,4 +59,42 @@ Proof. reflexivity. Qed.
 Example test_orb4: (orb true true) = true.
 Proof. reflexivity. Qed.
 
+Definition nandb (b1:bool) (b2:bool) : bool :=
+    match b1 with
+    | false => true
+    | true => negb b2
+    end.
 
+Example test_nandb1: (nandb true false) = true.
+Proof. reflexivity. Qed.
+
+Example test_nandb2: (nandb false false) = true.
+Proof. reflexivity. Qed.
+
+Example test_nandb3: (nandb false true) = true.
+Proof. reflexivity. Qed.
+
+Example test_nandb4: (nandb true true) = false.
+Proof. reflexivity. Qed.
+
+Definition andb3 (b1:bool) (b2:bool) (b3:bool) : bool :=
+    match b1 with
+    | true => andb b2 b3
+    | false => false
+    end.
+
+Example test_andb31: (andb3 true true true) = true.
+Proof. reflexivity. Qed.
+
+Example test_andb32: (andb3 false true true) = false.
+Proof. reflexivity. Qed.
+
+Example test_andb33: (andb3 true false true) = false.
+Proof. reflexivity. Qed.
+
+Example test_andb34: (andb3 true true false) = false.
+Proof. reflexivity. Qed.
+
+Check true.
+Check (negb true).
+Check negb.
