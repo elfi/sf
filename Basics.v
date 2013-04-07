2f041369245182f9e45808750c2104379418ddc5
Miroslav Dobsicek <m.dobsicek@gmail.com>
2013-04-07 22:48:01 +0200

    Next bits

--- Basics.v
+++ Basics.v
@@ -98,3 +98,108 @@ Proof. reflexivity. Qed.
 Check true.
 Check (negb true).
 Check negb.
+
+Module Playground1.
+
+Inductive nat : Type :=
+| O : nat
+| S : nat -> nat.
+
+Definition pred (n:nat) : nat :=
+    match n with
+    | O => O
+    | S n' => n'
+    end.
+
+End Playground1.
+
+Definition minustwo (n:nat) : nat :=
+    match n with
+    | O => O
+    | S O => O
+    | S (S n') => n'
+    end.
+
+Check (S (S (S (S O)))).
+Eval simpl in (minustwo 4).
+
+Check S.
+Check pred.
+Check minustwo.
+
+Fixpoint evenb (n:nat) : bool :=
+    match n with
+    | O => true
+    | S O => false
+    | S (S n') => evenb n'
+    end.
+
+Definition oddb (n:nat) : bool :=
+    negb (evenb n).
+
+Example test_oddb1: (oddb (S O)) = true.
+Proof. reflexivity. Qed.
+
+Example test_oddb2: (oddb (S (S (S (S O))))) = false.
+Proof. reflexivity. Qed.
+
+Module Playground2.
+
+Fixpoint plus (n:nat) (m:nat) : nat :=
+    match n with
+    | O => m
+    | S n' => S (plus n' m)
+    end.
+
+Eval simpl in (plus (S (S (S O))) (S (S O))).
+
+Fixpoint mult (n m:nat) : nat :=
+    match n with
+    | O => O
+    | S n' => plus m (mult n' m)
+    end.
+
+Example test_mult1: (mult 3 3) = 9.
+Proof. reflexivity. Qed.
+
+Fixpoint minus (n m:nat) : nat :=
+    match n, m with
+    | O, _ => O
+    | n, O => n
+    | S n', S m' => minus n' m'
+    end.
+
+Eval simpl in (minus 5 3).
+
+End Playground2.
+
+Fixpoint exp (base power:nat) : nat :=
+    match power with
+    | O => 1
+    | S n' => mult base (exp base n')
+    end.
+
+Fixpoint factorial (n:nat) : nat :=
+    match n with
+    | O => 1
+    | S n' => mult n (factorial n')
+    end.
+
+Example test_factorial1: (factorial 3) = 6.
+Proof. simpl. reflexivity. Qed.
+
+Example test_factorial2: (factorial 5) = (mult 10 12).
+Proof. simpl. reflexivity. Qed.
+
+Notation "x + y" := (plus x y)
+    (at level 50, left associativity)
+    : nat_scope.
+Notation "x - y" := (minus x y)  
+    (at level 50, left associativity)
+    : nat_scope.
+Notation "x * y" := (mult x y)  
+    (at level 40, left associativity) 
+    : nat_scope.
+
+Check ((0 + 1) + 1).
+
