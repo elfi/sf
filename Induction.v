24aeb613461ae122ec755fdbef1a9085954d0e2c
Miroslav Dobsicek <m.dobsicek@gmail.com>
2013-05-07 22:05:59 +0200

    More induction examples

--- Induction.v
+++ Induction.v
@@ -69,3 +69,53 @@ Proof.
     Case "n = S n'". simpl. rewrite -> IHn'. reflexivity.
 Qed.
 
+Theorem mult_0_r : forall n:nat,
+    n * 0 = 0.
+Proof.
+    intro n. induction n as [| n'].
+    Case "n = 0". reflexivity.
+    Case "n = S n'". simpl. rewrite -> IHn'. reflexivity.
+Qed.
+
+Theorem plus_n_Sm : forall n m : nat,
+    S (n + m) = n + (S m).
+Proof.
+    intros n m.  induction n as [| n'].
+    Case "n = 0". simpl. reflexivity.
+    Case "n = S n'".  simpl. rewrite -> IHn'. reflexivity.
+Qed.
+
+Theorem plus_comm : forall n m : nat,
+    n + m = m + n.
+Proof.
+    intros n m. induction n as [| n'].
+    Case "n = 0".
+        simpl. rewrite -> plus_0_r. reflexivity.
+    Case "n = S n'".
+        simpl. rewrite -> IHn'. rewrite -> plus_n_Sm. reflexivity.
+Qed.
+
+Theorem plus_assoc : forall n m p : nat,
+    n + (m + p) = (n + m) + p.
+Proof.
+    intros n m p.  induction n as [| n'].
+    Case "n = 0". simpl. reflexivity.
+    Case "n = S n'". simpl. rewrite -> IHn'. reflexivity.
+Qed.
+
+Fixpoint double (n:nat) :=
+    match n with
+    | O => O
+    | S n' => S (S (double n'))
+    end.
+
+Lemma double_plus : forall n:nat,
+    double n = n + n.
+Proof.
+    intro n. induction n as [| n'].
+    Case "n = 0".
+        simpl. reflexivity.
+    Case "n = S n'".
+        simpl. rewrite -> IHn'. rewrite <- plus_n_Sm. reflexivity.
+Qed.
+
