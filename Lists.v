ef8700e46cb65c3a98bc287ee8c35402eab7583e
Miroslav Dobsicek <m.dobsicek@gmail.com>
2013-05-17 22:19:06 +0200

    Natlist

--- Lists.v
+++ Lists.v
@@ -41,4 +41,47 @@ Proof.
     simpl. reflexivity.
 Qed.
 
+Theorem snd_fst_is_swop : forall (p:natprod),
+    (snd p, fst p) = swap_pair p.
+Proof.
+    intro p. destruct p as (m,n).
+    simpl. reflexivity.
+Qed.
+
+Theorem fst_swap_is_snd : forall (p:natprod),
+    fst (swap_pair p) = snd p.
+Proof.
+    intro p. destruct p as (m,n).
+    simpl. reflexivity.
+Qed.
+
+Inductive natlist : Type :=
+| nil : natlist
+| cons : nat -> natlist -> natlist.
+
+Definition mylist := cons 1 (cons 2 (cons 3 nil)).
+
+Notation "x :: l" := (cons x l) (at level 60, right associativity).
+Notation "[ ]" := nil.
+Notation "[ x , .. , y ]" := (cons x .. (cons y nil) ..).
+
+
+Definition mylist1 := 1 :: (2 :: (3 :: nil)).
+Definition mylist2 := 1 :: 2 :: 3 :: nil.
+Definition mylist3 := [1,2,3].
+
+Fixpoint repeat (n count : nat) : natlist :=
+    match count with
+    | O => nil
+    | S count' => n :: (repeat n count')
+    end.
+
+Fixpoint length (l:natlist) : nat :=
+    match l with
+    | nil => 0
+    | x :: xs => S (lenght xs)
+    end.
+
+
+
 
