aa8a6a7cc02536ffa84eb33d4e9e1fa7352e1b11
Miroslav Dobsicek <m.dobsicek@gmail.com>
2013-05-25 23:11:32 +0200

    Implicit arguments

--- Poly.v
+++ Poly.v
@@ -54,4 +54,136 @@ Example test_rev2 :
     rev bool (nil bool) = nil bool.
 Proof. reflexivity. Qed.
 
+Module MumbleBaz.
+
+Inductive mumble : Type :=
+| a : mumble
+| b : mumble -> nat -> mumble
+| c : mumble.
+
+Inductive grumble (X:Type) : Type :=
+| d : mumble -> grumble X
+| e : X -> grumble X.
+
+(* Check (d (b a 5)).
+   (b a 5) has type mumble instead of Type *)
+Check (d mumble (b a 5)). (* grumble mumble*)
+Check (d bool (b a 5)).   (* grumble bool *)
+Check (e bool true).      (* grumble bool *)
+Check (e mumble (b c 0)). (* grumble mumble *)
+(* Check (e bool (b c 0)). 
+   (b c 0)  has type mumble instead of bool *)
+(* Check c.
+   type mumble, not grumble X *)
+
+Inductive baz: Type :=
+| x : baz -> baz
+| y : baz -> bool -> baz.
+
+(* baz has no elements, it cannot be constructed *)
+
+End MumbleBaz.
+
+Fixpoint app' X l1 l2 : list X :=
+    match l1 with
+    | nil => l2
+    | cons h t => cons X h (app' X t l2)
+    end.
+
+Check app'.
+Check app.
+
+Fixpoint length' (X : Type) (l : list X) : nat :=
+    match l with
+    | nil => 0
+    | cons h t => S (length' _ t)
+    end.
+
+Definition list123 :=
+    cons nat 1 (cons nat 2 (cons nat 3 (nil nat))).
+
+Definition list123' := cons _ 1 (cons _ 2 (cons _ 3 (nil _))).
+
+Implicit Arguments nil [[X]].
+Implicit Arguments cons [[X]].
+Implicit Arguments length [[X]].
+Implicit Arguments app [[X]].
+Implicit Arguments rev [[X]].
+Implicit Arguments snoc [[X]].
+
+Definition list123'' := cons 1 (cons 2 (cons 3 nil)).
+Check (length list123'').
+
+Fixpoint length'' {X : Type} (l : list X) : nat :=
+    match l with
+    | nil => 0
+    | cons h t => S (length'' t)
+    end.
+
+Definition mynil : list nat := nil.
+
+Check nil. (* nil : list ?64 *)
+Check @nil. (* @nil : forall X : Type, list X *)
+
+Definition mynil' := @nil nat.
+
+Notation "x :: y" := (cons x y)
+                     (at level 60, right associativity).
+Notation "[ ]" := nil.
+Notation "[ x , .. , y ]" := (cons x .. (cons y []) ..).
+Notation "x ++ y" := (app x y)
+                     (at level 60, right associativity).
+
+Definition list123''' := [1, 2, 3].
+
+Fixpoint repeat (X : Type) (n : X) (count : nat) : list X :=
+    match count with
+    | O => nil
+    | S count' => n :: (repeat X n count')
+    end.
+
+Example test_repeat1:
+    repeat bool true 2 = cons true (cons true nil).
+Proof. reflexivity. Qed.
+
+Theorem nil_app: forall X:Type, forall l:list X,
+    app [] l = l.
+Proof.
+    intros X l. simpl. reflexivity.
+Qed.
+
+Theorem rev_snoc: forall X : Type,
+                  forall v : X,
+                  forall s : list X,
+    rev (snoc s v) = v :: (rev s).
+Proof.
+    intros X v s. induction s as [| x xs].
+    Case "s = nil".
+        simpl. reflexivity.
+    Case "s = cons x xs".
+        simpl. rewrite -> IHxs. simpl. reflexivity.
+Qed.
+
+Theorem rev_involutive: forall X:Type, forall l:list X,
+    rev (rev l) = l.
+Proof.
+    intros X l. induction l as [| x xs].
+    Case "l = nil".
+        simpl. reflexivity.
+    Case "l = cons x xs".
+        simpl. rewrite -> rev_snoc. rewrite -> IHxs.
+        reflexivity.
+Qed.
+
+Theorem snoc_with_append: forall X : Type,
+                          forall l1 l2 : list X,
+                          forall v : X,
+    snoc (l1 ++ l2) v = l1 ++ (snoc l2 v).
+Proof.
+    intros X l1 l2 v. induction l1 as [| x xs].
+    Case "l1 = nil".
+        simpl. reflexivity.
+    Case "l1 = cons x xs".
+        simpl. rewrite -> IHxs. reflexivity.
+Qed.
 
