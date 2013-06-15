d1c3ec4b50585552ed07e25bf5ef6c383c74b0c2
Miroslav Dobsicek <m.dobsicek@gmail.com>
2013-06-15 23:18:19 +0200

    A family of propositions indexed by natural numbers

--- Prop.v
+++ Prop.v
@@ -1,2 +1,20 @@
 Require Export MoreCoq.
 
+Inductive beautiful : nat -> Prop :=
+| b_0 : beautiful 0
+| b_3 : beautiful 3
+| b_5 : beautiful 5
+| b_sum : forall n m, beautiful n -> beautiful m -> beautiful (n+m).
+
+Theorem three_is_beautiful: beautiful 3.
+Proof.
+    apply b_3.
+Qed.
+
+Theorem eight_is_beautiful: beautiful 8.
+Proof.
+    apply b_sum with (n:=3) (m:=5).
+    apply b_3.
+    apply b_5.
+Qed.
+
