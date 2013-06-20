3a45f6668d8e0c99821f2ce05239a6b37e550bcc
Miroslav Dobsicek <m.dobsicek@gmail.com>
2013-06-20 15:26:17 +0200

    Computational vs inductive definitions

--- Prop.v
+++ Prop.v
@@ -158,4 +158,81 @@ Proof.
         apply (g_plus5 (n' + m) IHHn).
 Qed.
 
+Theorem beautiful__gorgeous: forall n, beautiful n -> gorgeous n.
+Proof.
+    intros n H.
+    induction H as [| | | n' m' ].
+    Case "b_0".
+        apply g_0.
+    Case "b_3".
+        apply (g_plus3 0 g_0).
+    Case "b_5".
+        apply (g_plus5 0 g_0).
+    Case "b_sum".
+        apply (gorgeous_sum n' m' IHbeautiful1 IHbeautiful2).
+Qed.
+
+Lemma helper_g_times2: forall x y z, x + (z + y) = z + x + y.
+Proof.
+    intros x y z.
+    rewrite -> plus_swap.    
+    rewrite -> plus_assoc.
+    reflexivity.
+Qed.
+
+Theorem g_times2: forall n, gorgeous n -> gorgeous (2*n).
+Proof.
+    intros n H. simpl. rewrite -> plus_0_r.
+    induction H as [| n' | n' ].
+    Case "g_0".
+        simpl. apply g_0.
+    Case "g_plus3 n'".
+        rewrite -> helper_g_times2. simpl.
+        apply (g_plus3 (3 + n' + n') (g_plus3 (n' + n') IHgorgeous)).
+    Case "g_plus5 n'".
+        rewrite -> helper_g_times2. simpl.        
+        apply (g_plus5 (5 + n' + n') (g_plus5 (n' + n') IHgorgeous)).
+Qed.
+
+Definition even (n:nat) : Prop :=
+    evenb n = true.
+
+Check even.
+
+Inductive ev : nat -> Prop :=
+| ev_0 : ev 0
+| ev_SS : forall n:nat, ev n -> ev (S (S n)).
+
+Check ev.
+
+Theorem double_even: forall n,
+    ev (double n).
+Proof.
+    intro n. induction n as [| n'].
+    Case "n = O".
+        simpl. apply ev_0.
+    Case "n = S n'".
+        simpl. apply (ev_SS (double n') IHn').
+Qed.
+
+Theorem ev_minus2: forall n,
+    ev n -> ev (pred (pred n)).
+Proof.
+    intros n H.
+    destruct H as [| n' E' ].
+    Case "ev_0".
+        simpl. apply ev_0.
+    Case "ev_SS n' E'".
+        simpl. apply E'. 
+Qed.
+
+Theorem ev__even: forall n, ev n -> even n.
+Proof.
+    intros n E. induction E as [| n' E'].
+    Case "ev_0".
+        unfold even. simpl. reflexivity.
+    Case "ev_SS n' E'".
+        unfold even. simpl. apply IHE'.
+Qed.
+
 
