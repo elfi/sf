47a7aa314470753b9f94331411723cb42e28c09f
Miroslav Dobsicek <m.dobsicek@gmail.com>
2013-10-20 19:36:58 +0200

    Consequence

--- Hoare.v
+++ Hoare.v
@@ -88,3 +88,76 @@ Example hoare_asgn_examples_ex2:
     {{ (fun st => (0 <= st X /\ st X <= 5)) }}.
 Proof. apply hoare_asgn. Qed.
 
+Theorem hoare_asgn_fwd:
+    (forall {X Y : Type} {f g : X -> Y},
+       (forall (x:X), f x = g x) -> f = g) ->
+    forall m a Q,
+    {{ fun st => Q st /\ st X = m }}
+    X ::= a
+    {{ fun st => Q (update st X m) /\ 
+                 st X = aeval (update st X m) a }}.
+Proof.
+    intros functional_extensionality m a Q.
+    unfold hoare_triple. intros st st' Heval HQm.
+    inversion HQm.
+    inversion Heval. subst.
+    assert (Hst: st = update (update st X (aeval st a)) X (st X)).
+        apply functional_extensionality. intro x.
+        rewrite -> update_shadow. rewrite -> update_same;
+        reflexivity.
+    rewrite <- Hst.
+    split.
+    Case "left". assumption.
+    Case "right". unfold update. simpl. reflexivity.
+Qed.
+
+Theorem hoare_consequence_pre: forall (P P' Q : Assertion) c,
+    {{ P' }} c {{ Q }} ->
+    P ->> P' ->
+    {{ P }} c {{ Q }}.
+Proof.
+    intros P P' Q c Hhoare Himp. unfold hoare_triple.
+    intros st st' Hc HP. apply (Hhoare st st').
+    assumption.
+    apply Himp. assumption.
+Qed.
+
+Theorem hoare_consequence_post: forall (P Q Q' : Assertion) c,
+    {{ P }} c {{ Q' }} ->
+    Q' ->> Q ->
+    {{ P }} c {{ Q }}.
+Proof.
+    intros P Q Q' c Hhoare Himp. unfold hoare_triple.
+    intros st st' Hc HP. apply Himp. apply (Hhoare st st').
+    assumption. assumption.
+Qed.
+
+Example hoare_asgn_example1:
+    {{ fun st => True }}
+    X ::= (ANum 1)
+    {{ fun st => st X = 1 }}.
+Proof.
+    apply hoare_consequence_pre
+        with (P' := (fun st => st X = 1) [ X |-> ANum 1] ).
+    apply hoare_asgn.
+    unfold assert_implies. intros st Htrue.
+    unfold assn_sub. unfold update. simpl. reflexivity.
+Qed.
+
+Theorem hoare_consequence: forall (P P' Q Q' : Assertion) c,
+    {{ P' }} c {{ Q' }} ->
+    P ->> P' ->
+    Q' ->> Q ->
+    {{ P }} c {{ Q }}.
+Proof.
+    intros P P' Q Q' c Hhoare HPimp HQimp. unfold hoare_triple.
+    intros st st' Hceval HP.
+    apply HQimp. apply (Hhoare st st').
+    assumption.
+    apply HPimp. assumption.
+    (* or use:
+       apply hoare_consequence_pre with (P' := P')
+       apply hoare_consequence_post with (Q' := Q') *)
+Qed.
+
+
