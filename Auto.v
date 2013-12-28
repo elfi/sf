562ff501ab7a9a34e4fd48f99bc18338add16548
Miroslav Dobsicek <m.dobsicek@gmail.com>
2013-12-28 15:52:43 +0100

    Hint Unfold, Hint Constructors

--- Auto.v
+++ Auto.v
@@ -64,4 +64,43 @@ Example auto_example_6': forall n m p : nat,
     n = m.
 Proof. auto. Qed.
 
+Definition is_fortytwo x := x = 42.
+
+Example auto_example_7: forall x, (x <= 42 /\ 42 <= x) ->
+    is_fortytwo x.
+Proof.
+    auto.
+Abort.
+
+Hint Unfold is_fortytwo.
+
+Example auto_example_7': forall x, (x <= 42 /\ 42 <= x) ->
+    is_fortytwo x.
+Proof.
+    auto.
+Qed.
+
+Hint Constructors ceval.
+
+Definition st12 := update (update empty_state X 1) Y 2.
+Definition st21 := update (update empty_state X 2) Y 1.
+
+Example auto_example_8: exists s',
+    (IFB (BLe (AId X) (AId Y))
+         THEN (Z ::= AMinus (AId Y) (AId X))
+         ELSE (Y ::= APlus (AId X) (AId Z))
+     FI) / st21 || s'.
+Proof.
+   eexists. info_auto.
+Qed.
+
+Example auto_example_8': exists s',
+    (IFB (BLe (AId X) (AId Y))
+         THEN (Z ::= AMinus (AId Y) (AId X))
+         ELSE (Y ::= APlus (AId X) (AId Z))
+     FI) / st21 || s'.
+Proof.
+   eexists. info_auto.
+Qed.
+
 
